2ea26e02d5e99c84e1e060d2e09ce19b8be90f2d
Fernando Henrique <liquuid@gmail.com>
2020-05-01 00:52:11 -0300

    improovements  on cli, gui and debug mode

--- gox265.go
+++ gox265.go
@@ -11,20 +11,21 @@ import (
 	"strings"
 )
 
+func makeOutputName(inputName string, quality int, ext string) string {
+	return strings.Replace(inputName, "."+ext, ".x265_"+strconv.Itoa(quality)+".mkv", -1)
+}
+
 func main() {
-	//minusR := flag.Bool("r", false, "r")
+
+	minusP := flag.Bool("p", false, "p")
 	minusQ := flag.Int("q", 32, "q")
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Println("Usage: cmd <file extention>")
+		os.Exit(1)
+	}
 
 	list := make([]string, 0)
-
-	flag.Parse()
-
-	for index, val := range flag.Args() {
-		fmt.Println(index, ":", val)
-	}
-	//fmt.Println("-r :", *minusR)
-	//fmt.Println("-q :", string(*minusQ))
-
 	Ext := flag.Args()[0]
 
 	err := filepath.Walk(".", func(file string, info os.FileInfo, err error) error {
@@ -34,7 +35,6 @@ func main() {
 		}
 		if filepath.Ext(file) == "."+Ext {
 			list = append(list, file)
-			//fmt.Println("e", Ext, filepath.Ext(ext))
 		}
 
 		return nil
@@ -42,19 +42,28 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 	for i := range list {
 		dir := filepath.Dir(list[i])
 		file := filepath.Base(list[i])
-		fmt.Println(i, filepath.Dir(list[i]), " -> ", filepath.Base(list[i]))
-		cmd := exec.Command("ffmpeg", "-n", "-i", path.Join(dir, file), "-map", "0", "-c:a", "copy", "-c:s", "copy", "-c:v", "libx265", "-crf", strconv.Itoa(*minusQ), strings.Replace(path.Join(dir, file), ".mp4", ".x265_"+strconv.Itoa(*minusQ)+".mkv", -1))
+		inputName := path.Join(dir, file)
+		outputName := makeOutputName(inputName, *minusQ, Ext)
+
+		fmt.Println("(", i+1, "/", len(list), ")", filepath.Dir(list[i]), "/", filepath.Base(list[i]))
+		cmd := exec.Command("ffmpeg", "-n", "-i", inputName, "-map", "0", "-c:a", "copy", "-c:s", "copy", "-c:v", "libx265", "-crf", strconv.Itoa(*minusQ), outputName)
+
+		if *minusP {
+			cmd = exec.Command("echo", "ffmpeg", "-n", "-i", inputName, "-map", "0", "-c:a", "copy", "-c:s", "copy", "-c:v", "libx265", "-crf", strconv.Itoa(*minusQ), outputName)
+		}
+
 		stdout, err := cmd.Output()
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+			os.Exit(3)
 		}
 		fmt.Println(string(stdout))
+
 	}
 
 }
